7b96fbd997d123b6754498f72842d956c165e28d
James <52896018+jpm9rk@users.noreply.github.com>
2019-09-06 20:38:12 -0400

    Add files via upload

---
+++ spellcheck.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+ # James Morrissey
+ # computingID: jpm9rk
+
+import urllib.request
+spelled_word = []
+text = 'start'
+url = 'http://cs1110.cs.virginia.edu/files/words.txt'
+file_handle = urllib.request.urlopen(url)
+
+for line in file_handle:
+    line = line.strip().decode("UTF-8")
+    spelled_word.append(line)
+print("Type text; enter a blank line to end. ")
+while text != '':
+    text = input()
+    words = text.split()
+    for some_word in words:
+        stripped_word = some_word.strip(""".?'!",()""")
+        stripped_word = stripped_word.strip('')
+        lower_word = stripped_word.lower()
+        if (stripped_word in spelled_word) or (lower_word in spelled_word):
+            continue
+        else:
+            print("  MISSPELLED:", stripped_word)
+
+
